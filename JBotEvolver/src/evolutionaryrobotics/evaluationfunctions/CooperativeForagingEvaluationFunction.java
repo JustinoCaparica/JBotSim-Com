2eb8178adb6d890cd25f9c7755c0d00fc0016dd0
gus <gus@w3>
2016-08-31 12:56:25 +0100

    option added to turn disabled robots invisible to other robots

--- JBotEvolver/src/evolutionaryrobotics/evaluationfunctions/CooperativeForagingEvaluationFunction.java
+++ JBotEvolver/src/evolutionaryrobotics/evaluationfunctions/CooperativeForagingEvaluationFunction.java
@@ -25,7 +25,7 @@ public class CooperativeForagingEvaluationFunction extends EvaluationFunction{
 
     
     
-    public CooperativeForagingEvaluationFunction(Arguments args) {
+    public CooperativeForagingEvaluationFunction( Arguments args ) {
             super(args);	
             
             preysCaptured = 0;
@@ -37,7 +37,7 @@ public class CooperativeForagingEvaluationFunction extends EvaluationFunction{
     @Override
     public double getFitness() {
 
-            return  ( 1.0 * preysCaptured / preys ) / teamSize;
+            return  ( 1.0 * preysCaptured / preys );
     }
 
     
