10855f84f768ec15de0ff6a3626202734dc99ad6
gus <gus@w3>
2016-09-09 17:36:26 +0100

    Bug fixed by changing Set to List in classes RecruitActuator, RecruitedActuator and some others

---
+++ JBotEvolver/src/evolutionaryrobotics/EMain.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evolutionaryrobotics;
+
+/**
+ *
+ * @author gus
+ */
+public class EMain {
+    
+    public static void main(String[] args) throws Exception {
+        new EvolverMain(new String[]{"../../experiments/CooperativeForaging-NonSocial-SmallGroup-IncrementallEvolution/CooperativeForaging-NonSocial/1/_restartevolution.conf"});
+    }
+    
+}
--- JBotEvolver/src/evolutionaryrobotics/evaluationfunctions/CooperativeForagingDisableRobotsEvaluationFunction.java
+++ JBotEvolver/src/evolutionaryrobotics/evaluationfunctions/CooperativeForagingDisableRobotsEvaluationFunction.java
@@ -6,7 +6,7 @@ import simulation.environment.CooperativeForagingEnvironment;
 import simulation.robot.Robot;
 import simulation.util.Arguments;
 
-public class CooperativeForagingEvaluationFunction extends EvaluationFunction{
+public class CooperativeForagingDisableRobotsEvaluationFunction extends EvaluationFunction{
 	
     
     protected Vector2d   nestPosition = new Vector2d(0, 0);
@@ -21,10 +21,11 @@ public class CooperativeForagingEvaluationFunction extends EvaluationFunction{
     private int preysCaptured;              //number of preys captured
 
     
+    private Simulator simulator;
 
     
     
-    public CooperativeForagingEvaluationFunction( Arguments args ) {
+    public CooperativeForagingDisableRobotsEvaluationFunction( Arguments args ) {
             super(args);	
             
             preysCaptured = 0;
@@ -33,15 +34,38 @@ public class CooperativeForagingEvaluationFunction extends EvaluationFunction{
 
     
     
+    
+    
     @Override
     public double getFitness() {
-        return  ( 1.0 * preysCaptured / preys );
+
+        if ( simulator == null ) {                      //if the simulator has
+            return 0.0;                                 //not been initiated yet
+        }
+        
+        
+        int disabledRobots = 0;
+        for ( Robot robot : simulator.getRobots() ) {   //count the number
+            if ( !robot.isEnabled() ) {                 //of disabled robots
+                disabledRobots++;
+            }
+        }
+        
+                
+                
+        return  ( 1.0 * preysCaptured / preys ) / ( (1 + disabledRobots) * (1 + disabledRobots) ) ;
     }
 
     
     
+    
+    
+    
     @Override
     public void update(Simulator simulator) {
+
+        if( this.simulator == null )
+            this.simulator = simulator;
 
         //TODO next two lines are ugly, fix it when time is a surplus
         preys = ((CooperativeForagingEnvironment)(simulator.getEnvironment())).getNumberOfPreys(); 
