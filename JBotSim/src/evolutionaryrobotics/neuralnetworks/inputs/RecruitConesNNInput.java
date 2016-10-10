976e2e4359755e7358c30aad037cd3c4e80b5e43
gus <gus@w3>
2016-10-10 11:30:42 +0100

    added RecruitConesNNInput.java and RecruiterConesNNInput.java

---
+++ JBotSim/src/evolutionaryrobotics/neuralnetworks/inputs/RecruitConesNNInput.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evolutionaryrobotics.neuralnetworks.inputs;
+
+import simulation.robot.sensors.RecruitConesSensor;
+import simulation.robot.sensors.Sensor;
+
+/**
+ * NNInput to get the readings
+ * from the recruit cones sensor
+ * @author gus
+ */
+public class RecruitConesNNInput extends NNInput {
+
+    private final RecruitConesSensor recruitSensor;     //sensor that
+                                                        //perceives the recruited
+
+    /**
+     * Initializes a new instance
+     * @param s the sensor
+     * that this NNInput will
+     * use
+     */
+    public RecruitConesNNInput( Sensor s ) {
+        super(s);
+        this.recruitSensor = ( RecruitConesSensor ) s;
+    }
+    
+    @Override
+    public int getNumberOfInputValues() {
+        return 2;
+    }
+
+    @Override
+    public double getValue( int index ) {
+        
+        return recruitSensor.getSensorReading( index );
+        
+    }
+    
+}
---
+++ JBotSim/src/evolutionaryrobotics/neuralnetworks/inputs/RecruiterConesNNInput.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evolutionaryrobotics.neuralnetworks.inputs;
+
+import simulation.robot.sensors.RecruiterConesSensor;
+import simulation.robot.sensors.Sensor;
+
+/**
+ * NNInput to get the readings
+ * from the recruiter cones sensor
+ * @author gus
+ */
+public class RecruiterConesNNInput extends NNInput {
+
+    private RecruiterConesSensor recruiterSensor;           //sensor that
+                                                            //perceives the recruiter
+
+    /**
+     * Initializes a new instance
+     * @param s the sensor
+     * that this NNInput will
+     * use
+     */
+    public RecruiterConesNNInput( Sensor s ) {
+        super(s);
+        this.recruiterSensor = ( RecruiterConesSensor ) s;
+    }
+    
+    @Override
+    public int getNumberOfInputValues() {
+        return 2;
+    }
+
+    @Override
+    public double getValue( int index ) {
+        
+        return recruiterSensor.getSensorReading( index );
+        
+    }
+    
+}
