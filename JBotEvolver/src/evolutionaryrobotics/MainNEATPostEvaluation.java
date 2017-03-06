b471d4eb530d27ef579806a24616923933f37a00
guest <guest@fixo>
2017-03-06 16:06:17 +0000

    made sure that robots start at a safer distance from walls and other robots

---
+++ JBotEvolver/src/evolutionaryrobotics/MainNEATPostEvaluation.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evolutionaryrobotics;
+
+/**
+ * A class to execute post evaluation for
+ * NEAT networks
+ * @author guest
+ */
+public class MainNEATPostEvaluation {
+    
+    private static NEATPostEvaluation postEvaluator;
+    private static String[] arguments;
+
+    public MainNEATPostEvaluation() {
+        
+        
+    }
+    
+    
+    
+    
+    public static void main( String[] args ){
+        
+        
+        
+        arguments = new String[10];
+        
+        arguments[0] = "dir=../EvolutionAutomator/role-alloc-corridor/wallAndRobotSensor/roleAlloc-corridor-shortRange-NestRange/roleAlloc-corridor-shortRange-NestRange/1";
+        arguments[1] = "samples=10";
+        arguments[2] = "fitnesssamples=1";
+        arguments[3] = "targetfitness=0.0";
+        arguments[4] = "singleevaluation=1";
+        arguments[5] = "localevaluation=1";
+        arguments[6] = "steps=400";
+        arguments[7] = "showoutput=1";
+        arguments[8] = "sampleincrement=1";
+        arguments[9] = "saveoutput=1";
+        
+        
+        postEvaluator = new NEATPostEvaluation(arguments);
+        
+        
+        
+        
+        postEvaluator.runPostEval();
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+}
